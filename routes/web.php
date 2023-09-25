462f9501b4779a876a643f63ba683e01f92ac097
chinsukou <shun20020922@i.softbank.jp>
2023-09-25 13:51:17 +0900

    メール送信

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,6 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DifficultyController;
 use App\Http\Controllers\LikeController;
 
-Auth::routes(['verify' => true]);
-
 // 投稿用コントローラー
 Route::controller(PostController::class)->middleware(['auth', 'verified'])->group(function(){
     Route::get('/posts/create', 'create')->name('post.create');
