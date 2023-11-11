9a7904d3479402252480fa806e06e3df4c198e7e
chinsukou <shun20020922@i.softbank.jp>
2023-11-11 22:42:08 +0900

    フォローリストからプロフィールへの遷移

--- resources/views/user-prof/followed-list.blade.php
+++ resources/views/user-prof/followed-list.blade.php
@@ -11,11 +11,13 @@
       <div class='border-b border-gray-500 mb-5'>
       <div class='flex'>
         <div class='text-gray-500 py-5'>
+          <a href='/user-prof/prof-other/{{ $user->id }}'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
             stroke="currentColor" class="w-24 h-24">
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
           </svg>
+          </a>
         </div>
         <div class='py-8 ml-4'>
           <h2 class='text-lg mb-3'>名前：{{ $user->name }}</h2>
@@ -35,7 +37,7 @@
             <path stroke-linecap="round" stroke-linejoin="round"
               d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
           </svg>
-          <div class='items-center'>
+          <div class='flex items-center'>
             {{ $follower->name }}
           </div>
         </div>
